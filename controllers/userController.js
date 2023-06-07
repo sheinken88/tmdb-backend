75312e5e60beeaffe0170d95c82f9dd78888cd4d
PC <heinken.sebastian@gmail.com>
2023-06-07 08:16:06 -0300

    fixed getMovie import and added .env file

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,10 @@ const userService = require("../services/userService");
 
 const login = async (req, res) => {
   try {
-    const user = await userService.validateUserPassword(req.body.email, req.body.password);
+    const user = await userService.validateUserPassword(
+      req.body.email,
+      req.body.password
+    );
 
     if (!user) {
       return res.sendStatus(401);
@@ -21,7 +24,6 @@ const login = async (req, res) => {
 
     res.cookie("token", token);
     res.status(200).json(user);
-
   } catch (err) {
     res.status(404).send(err);
   }
@@ -29,10 +31,8 @@ const login = async (req, res) => {
 
 const signup = async (req, res) => {
   try {
-
     await userService.createUser(req.body);
-    res.sendStatus(200)
-
+    res.sendStatus(200);
   } catch (err) {
     res.status(404).send(err);
   }
@@ -59,13 +59,12 @@ const addToFavorites = async (req, res) => {
     const movieId = req.body.movieId;
 
     const newFavorites = await userService.addFavorite(userId, movieId);
-    
+
     if (newFavorites instanceof Error) {
       return res.status(newFavorites.status).send(newFavorites.message);
     }
 
     return res.status(200).send(newFavorites);
-
   } catch (error) {
     res.status(500).send("Error adding favorite");
   }
@@ -77,20 +76,16 @@ const removeFromFavorites = async (req, res) => {
     const movieId = req.body.movieId;
 
     const newFavorites = await userService.removeFavorite(userId, movieId);
-    
+
     if (newFavorites instanceof Error) {
       return res.status(newFavorites.status).send(newFavorites.message);
     }
 
     return res.status(200).send(newFavorites);
-
   } catch (error) {
     res.status(500).send("Error removing favorite");
   }
 };
-
-
-
 
 module.exports = {
   login,
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const validateUser = require("../middleware/auth");
 
-
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
@@ -11,6 +10,10 @@ router.post("/logout", userController.logout);
 router.get("/secret", userController.secret);
 router.get("/me", validateUser, userController.me);
 router.put("/:userId/addFavorite", validateUser, userController.addToFavorites);
-router.put("/:userId/removeFavorite", validateUser, userController.removeFromFavorites);
+router.put(
+  "/:userId/removeFavorite",
+  validateUser,
+  userController.removeFromFavorites
+);
 
 module.exports = router;
