06dc0c8c0896764571735c702687c9e57b108b6f
PC <heinken.sebastian@gmail.com>
2023-06-29 08:41:59 -0300

    change in cookie setup

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,7 +24,7 @@ const login = async (req, res) => {
 
     res.cookie("token", token, {
       secure: true,
-      httpOnly: true,
+      // httpOnly: true,
     });
 
     res.status(200).json(user);
