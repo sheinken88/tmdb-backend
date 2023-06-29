696334ba4648d4902144c65b1ba8fb6d663b5eed
PC <heinken.sebastian@gmail.com>
2023-06-29 11:18:49 -0300

    modified cookie setup for production

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,16 +22,12 @@ const login = async (req, res) => {
       favorites,
     });
 
-    res.cookie(
-      "token",
-      token
-      // {
-      //   secure: true,
-      //   httpOnly: true,
-      //   sameSite: "none",
-      //   domain: ".onrender.com",
-      // }
-    );
+    res.cookie("token", token, {
+      secure: true,
+      httpOnly: true,
+      sameSite: "none",
+      domain: ".onrender.com",
+    });
 
     res.status(200).json(user);
   } catch (err) {
